ce61933a719e6de44d9c9a6cd5cab9f0a1b48cb5
Nuelthesupreme <nuelthesupreme@gmail.com>
2020-09-07 21:57:54 +0100

    Updates

---
+++ precache-manifest.7ed9ec36cf3fba045b004bee347ffdfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebc951664c95416b8dcd25eaaf3414d",
+    "url": "/google-books-client/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136ca52d3b1f9b1d2f33",
+    "url": "/google-books-client/static/js/main.52dfb33c.chunk.js"
+  },
+  {
+    "revision": "2f4606b7ccc9863de05f",
+    "url": "/google-books-client/static/js/runtime-main.7a2912b8.js"
+  }
+]);
